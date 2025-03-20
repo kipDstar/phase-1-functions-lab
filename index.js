bcbc0e9fb87950647b577bf61b5ba2c4f6c36922
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-03-20 08:58:58 +0300

    updated index.js according to instruction

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+const distanceFromHqInFeet = function(feet) {
+    return distanceFromHqInBlocks(feet) * 264;
+}
+const distanceTravelledInFeet = function(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+const calculatesFarePrice = function(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    switch (true) {
+        case distance <= 400:
+            return 0;
+        case distance > 400 && distance <= 2000:
+            return (distance - 400) * 0.02;
+        case distance > 2000 && distance <= 2500:
+            return 25;
+        default:
+            return 'cannot travel that far';
+    }
+}
